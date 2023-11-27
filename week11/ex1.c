99cb582094bfb80152c4f627b6a0a7ea397e166d
Амир Губайдуллин <124494208+Amir23714@users.noreply.github.com>
2023-11-27 16:43:50 +0300

    Update ex1.c

--- week11/ex1.c
+++ week11/ex1.c
@@ -1,16 +1,20 @@
-#include <dirent.h>
 #include <stdio.h>
+#include <dirent.h>
 
 int main() {
-    // Open the root directory
     DIR *dir = opendir("./lofsdisk");
 
-    // Read and print the contents of the root directory
+    if (dir == NULL) {
+        perror("Error opening directory");
+        return 1;
+    }
+
     struct dirent *entry;
-    while ((entry = readdir(dir)) != NULL) {
+
+    for (; (entry = readdir(dir)) != NULL;) {
         printf("%s\n", entry->d_name);
     }
 
-    // Close the directory
     closedir(dir);
+    return 0;
 }
